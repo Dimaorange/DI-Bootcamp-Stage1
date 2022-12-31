f264f11ed2fd40bffbab804ae012a0b9f8f6bed7
Dima Issa <Dima>
2022-12-31 10:05:28 +0000

    Exo_Day3_Defi

---
+++ Week4/Day3/Exo_Defi/Defi_Day3.py
@@ -0,0 +1,41 @@
+#Defi 1
+#1.
+name=input("Entre votre nom:\n")
+#2.
+nam=[]
+for i in name:
+	if i in nam:
+		continue
+	else:
+		nam.append(i)
+dico={}
+for n in nam:
+	bat=[]
+	i=0
+	for ma in name:
+		if n==ma:
+			bat.append(i)
+		i+=1
+	dico.update({n:bat})
+print(dico)
+
+#Def 2.
+elem={"water":"$6",
+ "Pineapple":"$8", 
+ "Mangoes":"$3",
+ "Orange":"$4",
+ "Bananas":"$2", 
+ "Spoon":"$7",
+ "Fan":"$15",
+ "Apple":"$5",
+ }
+deco={}
+wallet="$"+input("Entre le montant de votre argent:\n")
+for cle,valeur in elem.items():
+	if elem[cle]<=wallet:
+ 		deco.update({cle:valeur})
+if deco!={}:
+	print(list(deco))
+	print(sorted(deco))
+else:
+	print("Nothing")
